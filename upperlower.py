afb47065ae174cf460efdae9cfbe11f4ff33a43b
anjukrishna07 <117815090+anjukrishna07@users.noreply.github.com>
2023-10-05 15:38:25 +0530

    Create upperlower.py

---
+++ upperlower.py
@@ -0,0 +1,16 @@
+class greeter:
+    def __init__(self,name):
+        self.name = name
+    def greet(self,loud=False):
+        if loud:
+            print("HELLO",self.name.upper())
+        else:
+            print("Hello",self.name.lower())
+g=  greeter('fred')
+g.greet()
+g.greet(loud=True)
+
+
+output:
+Hello fred
+HELLO FRED
